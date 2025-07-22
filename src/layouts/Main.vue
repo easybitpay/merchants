8720613050ce6bdbf2fa7e5e2ce931b01818bdef
Daniyal <daniyal.golpayegani@gmail.com>
2025-07-22 13:45:02 +0330

    hide sidebar

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -20,6 +20,7 @@ const appStore = useAppStore()
 // Refs
 const loading = ref(true)
 const sidebarStatus = ref(true)
+const showSidebar = ref(true)
 
 // Computeds
 const currentUser = computed(() => authStore.currentUser)
@@ -29,15 +30,22 @@ const changeSidebarStatus = () => {
   sidebarStatus.value = !sidebarStatus.value
 }
 
+const changeShowSidebar = () => {
+  if (sidebarStatus.value) {
+    changeSidebarStatus()
+  }
+  showSidebar.value = !showSidebar.value
+}
+
 /**
  * Set Email Verify Status
  */
- const setEmailVerifyStatus = () => {
+const setEmailVerifyStatus = () => {
   let emailVerifyAt = currentUser.value?.merchant?.email_verified_at
 
   if (!emailVerifyAt) {
     authStore.setShowEmailVerifyAlert(true)
-  }else {
+  } else {
     authStore.setShowEmailVerifyAlert(false)
   }
 }
@@ -60,13 +68,29 @@ onMounted(async () => {
 
 <template>
   <MainLoading v-if="loading" />
-  <div :class="`main-container sidebar-${sidebarStatus ? 'open' : 'close'}`">
+  <div
+    :class="[
+      { 'main-container': true },
+      { 'sidebar-open': sidebarStatus },
+      { 'sidebar-close': !sidebarStatus },
+      { 'sidebar-hide': !showSidebar }
+    ]"
+  >
     <Sidebar @changeSidebarStatus="changeSidebarStatus" :collapsed="!sidebarStatus" />
 
     <main id="main">
       <RouterView v-if="!loading" :key="$route.params.id" />
     </main>
   </div>
+
+  <button
+    type="button"
+    class="btn bg-white border-0 w-40px h-40px p-0 align-items-center d-flex d-sm-none shadow"
+    @click="changeShowSidebar"
+    style="position: fixed; top: 1rem; right: 1rem;"
+  >
+    <inline-svg :src="`/media/icons/icons/${showSidebar ? 'close' : 'grid'}.svg`"></inline-svg>
+  </button>
 
   <LockScreen />
 </template>
